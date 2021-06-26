fc9443ef1b72dfa574b161a7cc303529f89e3410
RJ <rjkaz@comcast.net>
2021-06-26 15:41:25 -0400

    need to seriously debug

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 document.getElementById("sad-face").addEventListener("click", function() {
+    //document.getElementsByClassName('face-container').className = 'face-container-sad';
+    let sadFace = document.getElementById("container");
+    sadFace.classList.remove("face-container");
+    sadFace.classList.add("face-container-sad");
     console.log("you shouldn't have done that!");
     document.getElementById("initial-face").src="imgs/sad.jpg";
     // this is the evil part
-    while(true) {
-        console.log("and again");
-    }
+    //while(true) {
+    //    console.log("and again");
+    // }
 })
