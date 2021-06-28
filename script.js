04de603aab1f6757207f05a67bd9a64a7d65b8be
Richard Kazanowski <rjkaz@comcast.net>
2021-06-28 16:31:35 -0400

    base function fix

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-document.getElementById("sad-face").addEventListener("click", function() {
-    //document.getElementsByClassName('face-container').className = 'face-container-sad';
-    let sadFace = document.getElementById("container");
-    sadFace.classList.remove("face-container");
-    sadFace.classList.add("face-container-sad");
-    console.log("you shouldn't have done that!");
-    document.getElementById("initial-face").src="imgs/sad.jpg";
-    // this is the evil part
-    //while(true) {
-    //    console.log("and again");
-    // }
-})
+
+// declaring a variable sadFace
+
+
+function evilFace() {
+    const sadFace = document.getElementById("container");
+    document.getElementById("sad-face-btn").addEventListener("click", function() {
+        sadFace.classList.remove("face-container");
+        sadFace.classList.add("face-container-sad");
+        console.log("you shouldn't have done that!");
+        document.getElementById("initial-face").src="imgs/sad.jpg";
+        //while(true) {
+        //    console.log("and again");
+        //}
+    });
+}
+
+evilFace();
+
+
